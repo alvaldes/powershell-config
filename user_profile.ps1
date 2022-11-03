33a7570a8d6d273989c7a8d45d5677ae5177113f
alvaldes <angelluis2605@gmail.com>
2022-11-03 01:28:37 -0400

    Upgrade and Add new Theme

--- user_profile.ps1
+++ user_profile.ps1
@@ -2,9 +2,8 @@
 [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
 
 Import-Module posh-git
-Import-Module oh-my-posh
-$omp_config = Join-Path $PSScriptRoot ".\takuya.omp.json"
-oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
+$omp_config = Join-Path $PSScriptRoot ".\amro.omp.json"
+oh-my-posh init pwsh --config $omp_config | Invoke-Expression
 
 Import-Module -Name Terminal-Icons
 
